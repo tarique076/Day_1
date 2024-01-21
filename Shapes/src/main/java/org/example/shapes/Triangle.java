82bb730f8ea280cb5387c1722c7d96bba12af420
Tarique Anwar <anwartarique408@gmail.com>
2024-01-21 10:14:09 +0530

    Test for triangle area added with corresponding method.

--- Shapes/src/main/java/org/example/shapes/Triangle.java
+++ Shapes/src/main/java/org/example/shapes/Triangle.java
@@ -10,10 +10,19 @@ public class Triangle {
         if(sideA==0 || sideB == 0 || sideC==0)
             throw new IllegalArgumentException();
 
+        if(sideA+sideB<=sideC || sideB+sideC<=sideA || sideA+sideC<=sideB){
+            throw new IllegalArgumentException();
+        }
+
         this.sideA = sideA;
         this.sideB = sideB;
         this.sideC = sideC;
     }
 
+    public double area(){
+        double semiPerimeter = (sideA+sideB+sideC)/2;
+
+        return Math.sqrt(semiPerimeter*(semiPerimeter-sideA)*(semiPerimeter-sideB)*(semiPerimeter-sideC));
+    }
 
 }
--- Shapes/src/test/java/TriangleTest.java
+++ Shapes/src/test/java/TriangleTest.java
@@ -1,6 +1,7 @@
 import org.example.shapes.Triangle;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class TriangleTest {
@@ -10,5 +11,27 @@ public class TriangleTest {
         assertThrows(IllegalArgumentException.class, () -> {
             new Triangle(0,10,20);
         });
+    }
+
+    @Test
+    public void expectTriangleCreationFailedIfSumOf2SideLessThanEqualTo3rdSide(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Triangle(5,10,30);
+        });
+    }
+    @Test
+    public void expectValidAreaForSides10_15And20(){
+        double semiPerimeter = (10+15+20)/2;
+        double expected = Math.sqrt(semiPerimeter*(semiPerimeter-10)*(semiPerimeter-15)*(semiPerimeter-20));
+
+        assertEquals(expected, new Triangle(10,15,20).area(),0.001);
+    }
+
+    @Test
+    public void expectValidAreaForSides5_12And15(){
+        double semiPerimeter = (10+15+20)/2;
+        double expected = Math.sqrt(semiPerimeter*(semiPerimeter-10)*(semiPerimeter-15)*(semiPerimeter-20));
+
+        assertEquals(expected, new Triangle(10,15,20).area(),0.001);
     }
 }
