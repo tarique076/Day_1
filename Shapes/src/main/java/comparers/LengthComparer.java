d7017c1ea198ce65027d97c203048db7eea926a5
Tarique Anwar <anwartarique408@gmail.com>
2024-01-22 02:19:53 +0530

    Added test for length conversion and corresponding method.

--- Shapes/src/main/java/comparers/LengthComparer.java
+++ Shapes/src/main/java/comparers/LengthComparer.java
@@ -13,4 +13,22 @@ public class LengthComparer {
         this.length2 = length2;
     }
 
+    public String compare(){
+        return null;
+    }
+
+    public double covertToMeter(String length){
+        String valueString = length.replaceAll("[^0-9.]","");
+        double value = Double.parseDouble(valueString);
+
+        if(length.contains("mm"))
+            return value/1000;
+        if(length.contains("cm"))
+            return value/100;
+        if(length.contains("km"))
+            return value*1000;
+
+        return value;
+
+    }
 }
--- Shapes/src/test/java/LengthComparerTest.java
+++ Shapes/src/test/java/LengthComparerTest.java
@@ -1,6 +1,7 @@
 import comparers.LengthComparer;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class LengthComparerTest {
@@ -10,5 +11,23 @@ public class LengthComparerTest {
         assertThrows(IllegalArgumentException.class, () ->{
            new LengthComparer("10", "10m");
         });
+    }
+
+    @Test
+    public void expect1000For1km(){
+        double expected = 1000.0;
+        assertEquals(expected, new LengthComparer("0m","0cm").covertToMeter("1km"), 0.1);
+    }
+
+    @Test
+    public void expect01For10cm(){
+        double expected = 0.1;
+        assertEquals(expected, new LengthComparer("0m","0cm").covertToMeter("10cm"), 0.1);
+    }
+
+    @Test
+    public void expect1mIsGreaterThan1cm(){
+        String expected = "Length1 1m is greater than length2 1cm.";
+        assertEquals(expected, new LengthComparer("1m", "1cm").compare());
     }
 }
